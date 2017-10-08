ecbea77bc676c76e66200c5dc51a86647f80b254
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-08 21:13:30 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,7 +8,7 @@ class FiguresController < ApplicationController
   get '/figures/new' do
     @titles = Title.all
     @landmarks = Landmark.all
-   erb :'figures/new'
+   erb :'/figures/new'
  end
 
  post '/figures' do
