f09e39500c8f5bd7f249b5ecf2108707f3a394ee
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-08 20:41:34 +0000

    automatically backed up by learn

--- db/migrate/20171008204056_create_figure_titles.rb
+++ db/migrate/20171008204056_create_figure_titles.rb
@@ -1,4 +1,9 @@
 class CreateFigureTitles < ActiveRecord::Migration
   def change
+    create_table :figure_titles do |f|
+      f.integer :title_id
+      f.integer :figure_id
+      
+    end
   end
 end
