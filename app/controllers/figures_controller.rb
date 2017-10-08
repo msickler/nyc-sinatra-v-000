3a2d2d46794ccb1fe15f0b0af7fb888fcd34a09a
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-08 20:51:18 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,19 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+   erb :'figures/new'
+ end
+
+ post '/figures' do
+    @figure = Figure.new(name: params["name"])
+    @figure.landmarks = Landmark.find_or_create_by(name: params["name"])
+    @figure.figure_titles = params[:figure_title]
+    @figure.save
+  end
+
 end
